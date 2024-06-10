678115d59b5879be258822a3ddee2f8ccd461ebc
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-06-10 11:29:06 -0600

    1

---
+++ final/src/App.java
@@ -0,0 +1,21 @@
+// Welcome to "First Day of Work"
+
+
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int Health, DressCode, Cleanliness, Punctuality, Professionalism, CustomerService;
+    
+        Scanner keys = new Scanner(System.in);
+
+        // indicate your answer using the option' letter, e.g, A.
+        System.out.print("You've just been sent an email with important information about your new job starting tomorrow. A) open it B) ignore it");
+        String one = keys.nextLine();
+        if (one == "A"){
+            
+        }
+    
+    }
+
+
+}
