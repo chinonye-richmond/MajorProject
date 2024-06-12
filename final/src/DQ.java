72cf2cf0344b89124d58b1f30731cb08147118d5
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-06-12 11:25:40 -0600

    f

--- final/src/DQ.java
+++ final/src/DQ.java
@@ -1,24 +1,49 @@
 import java.util.Scanner;
 public class DQ {
-    int[] points = {0,0,0,0,0,0};
-    String[] ptype = {"Cleanliness", "Punctuality", "DressCode", "CustomerService", "Health", "Professionalism"};
+    private int numofq = 30;
+    private int[] Cleanliness = new int[numofq];
+    private int[] Punctuality = new int[numofq];
+    private int[] DressCode = new int[numofq];
+    private int[] Health = new int[numofq];
+    private int[] Professionalism = new int[numofq];
+    private int[] CustomerService = new int[numofq];
     
-    Scanner keys = new Scanner(System.in);
+    //String[] ptype = {"Cleanliness", "Punctuality", "DressCode", "CustomerService", "Health", "Professionalism"};
+    
+    private Scanner keys = new Scanner(System.in);
         
     public void email(){
         System.out.println("You've just been sent an email to help you prep for your first say of work tomorrow. A) open it B) ignore it ");
         // indicate your answer using the option's corresponding letter
         String one = keys.nextLine();
         if (one == "A"){
-            for(int i=0; i<points.length;i++){
-                points[i] = points[i] + 
-
-            }
-            
-
+            Cleanliness[0] = rand(50,100); 
+            Punctuality[0] = rand(50,100);
+            DressCode[0] = rand(50,100);
+            Health[0] = rand(50,100);
+            Professionalism[0] = rand(50,100);
+            CustomerService[0] = rand(50,100);        
+        }
+        else if (one == "B"){
+            Cleanliness[0] = rand(0,50); 
+            Punctuality[0] = rand(0,50);
+            DressCode[0] = rand(0,50);
+            Health[0] = rand(0,50);
+            Professionalism[0] = rand(0,50);
+            CustomerService[0] = rand(0,50);
         }
 
+    }
+    private void Average(int[] array){
+        if (int[] array == Cleanliness)
+        for(int i=0;i<Cleanliness.length;i++){
 
+
+        }
+    }
+    public void PrintStats(){
+
+        System.out.println();
     }
 
     
